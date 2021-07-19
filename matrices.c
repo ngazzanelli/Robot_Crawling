d6ba81e40b3fe85c3ffbc7e6d40dc86b1b108002
nicco <niccogazza@hotmail.it>
2021-07-19 17:44:07 +0200

    matrix library

--- matrices.c
+++ matrices.c
@@ -28,10 +28,8 @@ typedef struct {
     float dq5;
     float dq6;
 } dot_state;
-dot_state dot_robot;
 
-
-void update_kyn(gsl_matrix *Tsee, state robot){
+void update_kyn(float Tsee[4][4], state robot){
 	float value;
 	float q1, q2, q3, q4, q5, q6;
 	q1 = robot.q1;
@@ -43,13 +41,13 @@ void update_kyn(gsl_matrix *Tsee, state robot){
 
 
 	value = cos(q4 + q5)*sin(q3) + cos(q3)*sin(q4 + q5);
-	gsl_matrix_set(Tsee, 0, 0, value);
+	Tsee[0][0] = value;
 
   	value = cos(q3)*cos(q4 + q5) - sin(q3)*sin(q4 + q5);
-  	gsl_matrix_set(Tsee, 0, 1, value);
+  	Tsee[0][1] =  value;
 
   	value = 0.0;
-  	gsl_matrix_set(Tsee,0,2,value);
+  	Tsee[0][2] = value;
 
 
   	value = 1.0*(-0.1 + q1 + 0.1*cos(q3) - 0.03*sin(q3)) +
@@ -57,38 +55,38 @@ void update_kyn(gsl_matrix *Tsee, state robot){
 			cos(q4)*(0.06 + 0.06*sin(q5))) -
 			sin(q3)*(0.015 - 0.06*cos(q4)*cos(q5) +
 			sin(q4)*(0.06 + 0.06*sin(q5)));
-	gsl_matrix_set(Tsee, 0, 3, value);
+	Tsee[0][3] = value;
 
 
 	value = 0.0 - cos(q3)*cos(q4 + q5) + sin(q3)*sin(q4 + q5);
-	gsl_matrix_set(Tsee, 1, 0, value);
+	Tsee[1][0] = value;
 
 	value = cos(q4 + q5)*sin(q3) + cos(q3)*sin(q4 + q5);
-	gsl_matrix_set(Tsee, 1, 1, value);
+	Tsee[1][1] =  value;
 
 	value = 0.0;
-	gsl_matrix_set(Tsee, 1, 2, value);
+	Tsee[1][2] =  value;
 
 	value = 1.0*(0.015 + q2 + 0.03*cos(q3) + 0.1*sin(q3)) + 
 		sin(q3)*(0.1 + 0.06*cos(q5)*sin(q4) +
 		cos(q4)*(0.06 + 0.06*sin(q5))) +
 		cos(q3)*(0.015 - 0.06*cos(q4)*cos(q5) +
 		sin(q4)*(0.06 + 0.06*sin(q5)));
-	gsl_matrix_set(Tsee, 1, 3, value);
+	Tsee[1][3] =  value;
 
 	//Terza e quarta riga
-	gsl_matrix_set(Tsee, 2, 0, 0.0);
-	gsl_matrix_set(Tsee, 2, 1, 0.0);
-	gsl_matrix_set(Tsee, 2, 2, 1.0);
-	gsl_matrix_set(Tsee, 2, 3, 0.0);
+	Tsee[2][0] =  0.0;
+	Tsee[2][1] =  0.0;
+	Tsee[2][2] =  1.0;
+	Tsee[2][3] =  0.0;
 
-	gsl_matrix_set(Tsee, 3, 0, 0.0);
-	gsl_matrix_set(Tsee, 3, 1, 0.0);
-	gsl_matrix_set(Tsee, 3, 2, 0.0);
-	gsl_matrix_set(Tsee, 3, 3, 1.0);
+	Tsee[3][0] = 0.0;
+	Tsee[3][1] = 0.0;
+	Tsee[3][2]= 0.0;
+	Tsee[3][3] = 1.0;
 }
 
-void update_S2(gsl_matrix *S2, state robot){
+void update_S2(float S2[4][2], state robot){
 	float value;
 	float q3, q4, q5;
 	q3 = robot.q3;
@@ -102,35 +100,35 @@ void update_S2(gsl_matrix *S2, state robot){
 	0.0009*sin(q4)*sin(q5))*(-0.06*cos(q3 + q4) -
 	0.06*sin(q3 + q4 + q5)))/(0.003 + 0.0009*cos(q5)*sin(q4) +
 	cos(q4)*(0.0009 + 0.0009*sin(q5)));
-	gsl_matrix_set(S2, 0, 0, value);
+	S2[0][0] = value;
 
 	value =  -0.06*cos(q3 + q4 + q5) - 
 	(0.03*(0.5 + 1.0*cos(q3) - 2.0*cos(q4)*cos(q5) + 3.33333*sin(q3) +
 	2.0*sin(q4) + 2.0*sin(q4)*sin(q5))*sin(q3 + q4 + q5))/(3.33333 + 1.0*cos(q5)*sin(q4) +
 	cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_matrix_set(S2, 0, 1, value);
+	S2[0][1] = value;
 
 	//SECONDA RIGA
 	value = ((0.0015 - 0.0015*cos(q3) +
 	0.00045*sin(q3))*(-0.06*cos(q3 + q4) - 0.06*sin(q3 + q4 + q5)))/(
 	0.003 + 0.0009*cos(q5)*sin(q4) +
 	cos(q4)*(0.0009 + 0.0009*sin(q5)));
-	gsl_matrix_set(S2, 1, 0, value);
+	S2[1][0] = value;
 
 	value = (0.1*(-1.0 + 1.0*cos(q3) - 0.3*sin(q3))*sin(q3 + q4 + q5))/(
 	3.33333 + 1.0*cos(q5)*sin(q4) +
 	cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_matrix_set(S2, 1, 1, value);
+	S2[1][1] = value;
 
 	//TERZA RIGA
 	value = (-1.0*cos(q3 + q4) -
 	1.0*sin(q3 + q4 + q5))/(
 	3.33333 + 1.0*cos(q5)*sin(q4) + cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_matrix_set(S2, 2, 0, value);
+	S2[2][0] = value;
 
 	value =  -((1.0*sin(q3 + q4 + q5))/(
 	3.33333 + 1.0*cos(q5)*sin(q4) + cos(q4)*(1.0 + 1.0*sin(q5))));
-	gsl_matrix_set(S2, 2, 1, value);
+	S2[2][1] = value;
 
 	//QUARTA RIGA
 	value = (3.0*cos(q3 + q4) - 4.0*cos(q3 - q5) +
@@ -138,37 +136,37 @@ void update_S2(gsl_matrix *S2, state robot){
 	8.0*sin(q3) - (13.3333)*sin(q3 + q4) +
 	3.0*sin(q3 + q4 + q5))/(
 	3.33333 + 1.0*cos(q5)*sin(q4) + cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_matrix_set(S2, 3, 0, value);
+	S2[3][0] = value;
 
 
 	value = (4.0*cos(q3 + q5) + (13.3333)*cos(q3 + q4 + q5) -
 	4.0*sin(q3) + 3.0*sin(q3 + q4 + q5))/(3.33333 + 1.0*cos(q5)*sin(q4) +
 	cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_matrix_set(S2, 3, 1, value);
+	S2[3][1] = value;
 }
 
 
-void update_M1(gsl_matrix *M1, state robot){
+void update_M1(float M1[2][2], state robot){
 	float value;
 	float q5;
 	q5 = robot.q5;
 
 	//PRIMA RIGA
 	value = 0.0000822813 + 0.000018*sin(q5);
-	gsl_matrix_set(M1, 0, 0, value);
+	M1[0][0] = value;
 
 	value = 0.0000231406 + 9.*pow(10, -6)*sin(q5);
-	gsl_matrix_set(M1, 0, 1, value);
+	M1[0][1] = value;
 
 	//SECONDA RIGA
 	value = 0.0000231406 + 9.*pow(10, -6)*sin(q5);
-	gsl_matrix_set(M1, 1, 0, value);
+	M1[1][0] = value;
 
 	value = 0.0000231406;
-	gsl_matrix_set(M1, 1, 1, value);
+	M1[1][1] = value;
 }
 
-void update_G1(gsl_vector *G1, state robot){
+void update_G1(float G1[2], state robot){
 	float value;
 	float q3, q4, q5;
 	q3 = robot.q3;
@@ -178,14 +176,14 @@ void update_G1(gsl_vector *G1, state robot){
 	//PRIMA RIGA
 	value = 0.0 + 0.00441*cos(q3)*cos(q4) + 0.00294*cos(q3 + q4) -
 	0.00441*sin(q3)*sin(q4) + 0.00147*sin(q3 + q4 + q5);
-	gsl_vector_set(G1, 0, value);
+	G1[0] = value;
 
 	//SECONDA RIGA
 	value = 0.0 + 0.00147*sin(q3 + q4 + q5);
-	gsl_vector_set(G1, 1, value);
+	G1[1] = value;
 }
 
-void update_C1(gsl_matrix *C1, state robot, dot_state dot_robot){
+void update_C1(float C1[2][2], state robot, dot_state dot_robot){
 	float value;
 	float q3, q4, q5, dotq1, dotq2, dotq3, dotq4, dotq5;
 	q3 = robot.q3;
@@ -207,14 +205,14 @@ void update_C1(gsl_matrix *C1, state robot, dot_state dot_robot){
 	0.00015*dotq2*sin(q3 + q4) + 0.0000375*dotq3*sin(q3 + q4) -
 	2.25*pow(10, -6)*dotq3*sin(q4 + q5) + 0.000075*dotq1*sin(q3 + q4 + q5) -
 	1.125*pow(10, -6)*dotq3*sin(q3 + q4 + q5);
-	gsl_matrix_set(C1, 0, 0, value);
+	C1[0][0] = value;
 
 	value = 9.*pow(10, -6)*dotq5*cos(q5) - 7.5*pow(10, -6)*dotq3*cos(q4 + q5) -
 	0.000075*dotq2*cos(q3 + q4 + q5) -
 	7.5*pow(10, -6)*dotq3*cos(q3 + q4 + q5) -
 	2.25*pow(10, -6)*dotq3*sin(q4 + q5) + 0.000075*dotq1*sin(q3 + q4 + q5) -
 	1.125*pow(10, -6)*dotq3*sin(q3 + q4 + q5);
-	gsl_matrix_set(C1, 0, 1, value);
+	C1[0][1] = value;
 
 	//SECONDA RIGA
 	value = -9.*pow(10, -6)*dotq4*cos(q5) +
@@ -223,7 +221,7 @@ void update_C1(gsl_matrix *C1, state robot, dot_state dot_robot){
 	1.66667*cos(q4 + q5) + 1.66667*cos(q3 + q4 + q5) +
 	0.5*sin(q4 + q5) + 0.25*sin(q3 + q4 + q5)) +
 	0.000075*dotq1*sin(q3 + q4 + q5);
-	gsl_matrix_set(C1, 1, 0, value);
+	C1[1][0] = value;
 
 	value = -4.5*pow(10, -6)*dotq4*cos(q5) -
 	4.5*pow(10, -6)*dotq3*cos(q3 + q5) - 7.5*pow(10, -6)*dotq3*cos(q4 + q5) -
@@ -231,10 +229,10 @@ void update_C1(gsl_matrix *C1, state robot, dot_state dot_robot){
 	7.5*pow(10, -6)*dotq3*cos(q3 + q4 + q5) -
 	2.25*pow(10, -6)*dotq3*sin(q4 + q5) + 0.000075*dotq1*sin(q3 + q4 + q5) -
 	1.125*pow(10, -6)*dotq3*sin(q3 + q4 + q5);
-	gsl_matrix_set(C1, 1, 1, value);
+	C1[1][1] = value;
 }
 
-void update_G2(gsl_vector *G2, state robot){
+void update_G2(float G2[2], state robot){
 	float value;
 	float q3, q4, q5;
 	q3 = robot.q3;
@@ -252,7 +250,7 @@ void update_G2(gsl_vector *G2, state robot){
 	0.06*sin(q3 + q4 + q5)))/(0.003 + 0.0009*cos(q5)*sin(q4) +
 	cos(q4)*(0.0009 + 0.0009*sin(q5))) +
 	0.00147*sin(q3 + q4 + q5);
-	gsl_vector_set(G2, 0, value);
+	G2[0] =  value;
 
 	//SECONDA RIGA
 	value = 0.0 + 0.00147*sin(q3 + q4 + q5) + 
@@ -262,11 +260,10 @@ void update_G2(gsl_vector *G2, state robot){
 	0.00147*cos(q5)*sin(q4) +
 	cos(q4)*(0.00735 + 0.00147*sin(q5)))*sin(q3 + q4 + q5))/(
 	3.33333 + 1.0*cos(q5)*sin(q4) + cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_vector_set(G2, 1, value);
+	G2[1] = value;
 }
 
-
-void update_M2(gsl_matrix *M2, state robot){
+void update_M2(float M2[2][2], state robot){
  	float value;
  	float q1, q2, q3, q4, q5, q6;
  	q1 = robot.q1;
@@ -354,7 +351,7 @@ void update_M2(gsl_matrix *M2, state robot){
 	 4.44444*cos(q3 + q4 + q5) - 2.66667*sin(q3) -
 	 4.44444*sin(q3 + q4) + 1.0*sin(q3 + q4 + q5)))/pow((3.33333 +
 	 1.0*cos(q5)*sin(q4) + cos(q4)*(1.0 + 1.0*sin(q5))), 2);
-	 gsl_matrix_set(M2, 0, 0, value);
+	 M2[0][0] = value;
 
 //M2(0,1)
 	value = 1/pow((3.33333 + 1.0*cos(q4) + 1.0*sin(q4 + q5)),2)*(0.0165437 -
@@ -377,7 +374,7 @@ void update_M2(gsl_matrix *M2, state robot){
 	 0.0000372813*sin(q3 + 2*q4 + q5) -
 	 0.00871572*sin(2*q3 + 2*q4 + q5) - 0.0003906*sin(q4 + 2*q5) +
 	 0.0007812*sin(2*q3 + q4 + 2*q5));
-	 gsl_matrix_set(M2, 0, 1, value);
+	M2[0][1] = value;
 
 //M2(1,0)
 	value = 1/pow((3.33333 + 1.0*cos(q4) +
@@ -401,7 +398,7 @@ void update_M2(gsl_matrix *M2, state robot){
  0.0000372812*sin(q3 + 2*q4 + q5) -
  0.00871572*sin(2*q3 + 2*q4 + q5) - 0.0003906*sin(q4 + 2*q5) +
  0.0007812*sin(2*q3 + q4 + 2*q5));
- 	gsl_matrix_set(M2, 1, 0, value);
+	M2[1][0] = value;
 
 //M2(1,1)
 	value = (0.0155731 -
@@ -420,10 +417,10 @@ void update_M2(gsl_matrix *M2, state robot){
  0.00001575*sin(2*q3 + 2*q4 + q5) +
  0.0007812*sin(2*q3 + q4 + 2*q5))/pow((3.33333 + 1.0*cos(q5)*sin(q4) +
  cos(q4)*(1.0 + 1.0*sin(q5))), 2);
- 	gsl_matrix_set(M2, 1, 1, value);
+	M2[1][1] = value;
 }
 
-void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
+void update_C2(float C2[2][2], state robot, dot_state dot_robot){
 
 	float value;
 	float q1, q2, q3, q4, q5, q6;
@@ -913,7 +910,7 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  1.0*cos(q5)*sin(q4) +
  cos(q4)*(1.0 + 1.0*sin(q5))), 2))))/(3.33333 +
  1.0*cos(q5)*sin(q4) + cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_matrix_set(C2, 0, 0, value);
+	C2[0][0] = value;
 
 //C2(0,1)
 	value = 9.*pow(10, -6)*dotq5*cos(q5) - 0.000075*dotq2*cos(q3 + q4 + q5) +
@@ -1199,7 +1196,6 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  cos(q4)*(1.0 + 1.0*sin(q5))), 2) - (0.03*(0.00369 -
  0.01467*cos(q3) + 0.00015*cos(q4)*cos(q5) -
  0.0489*sin(q3) - 0.00075*sin(q4) -
-
  0.00015*sin(q4)*sin(
  q5))*(1.0*dotq5*(1.0*cos(
  q3 + q4 + q5)*(0.5 + 1.0*cos(q3) -
@@ -1304,7 +1300,7 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  1.0*cos(2*q3 + q4 + q5) +
  0.3*sin(2*q3 + q4 + q5))))/pow((3.33333 + 1.0*cos(q5)*sin(q4) + 
  cos(q4)*(1.0 + 1.0*sin(q5))), 2);
- 	gsl_matrix_set(C2, 0, 1, value);
+ 	C2[0][1] = value;
 
 //C2(1,0)
 	value = -9.*pow(10, -6)*dotq4*cos(q5) +
@@ -1826,7 +1822,7 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  cos(q4)*(1.0 + 1.0*sin(q5))), 2))))/(3.33333 +
  1.0*cos(q5)*sin(q4) +
  cos(q4)*(1.0 + 1.0*sin(q5)));
-	gsl_matrix_set(C2, 1, 0, value);
+	C2[1][0] = value;
 
 //C2(1,1)
 
@@ -1941,7 +1937,6 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  q3 + q4 + q5) -
  2.0*pow((3.33333 + 1.0*cos(q5)*sin(q4) +
  cos(q4)*(1.0 + 1.0*sin(q5))), 2)*sin(q3 + q4 + q5)) +
-
  1.0*dotq4*(1.0*cos(
  q3 + q4 + q5)*(0.5 + 1.0*cos(q3) -
  2.0*cos(q4)*cos(q5) + 3.33333*sin(q3) + 2.0*sin(q4) +
@@ -1991,7 +1986,6 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  q3 + q5) + 6.66667*cos(q3 + q4 + q5) +
  1.0*cos(q3 + 2*q4 + q5) - 2.0*sin(q3))*(3.33333 -
  3.33333*cos(q3) + 1.0*sin(q3)) -
-
  0.03*dotq4*(1.0*cos(q3 + q5) +
  3.33333*cos(q3 + q4 + q5) - 1.0*sin(q3))*(3.33333 -
  3.33333*cos(q3) + 1.0*sin(q3)) +
@@ -2160,7 +2154,6 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  3.33333*sin(q3) + 2.0*sin(q4) +
  2.0*sin(q4)*sin(q5))*(3.33333 + 1.0*cos(q5)*sin(q4) +
  cos(q4)*(1.0 + 1.0*sin(q5))) +
-
  2.0*(1.0*cos(q4)*cos(q5) - 1.0*sin(q4)*sin(q5))*(-0.25 -
  0.5*cos(q3) + 1.0*cos(q4)*cos(q5) - 1.66667*sin(q3) -
  1.0*sin(q4) - 1.0*sin(q4)*sin(q5))*sin(q3 + q4 + q5) +
@@ -2201,42 +2194,72 @@ void update_C2(gsl_matrix *C2, state robot, dot_state dot_robot){
  1.0*cos(2*q3 + q4 + q5) +
  0.3*sin(2*q3 + q4 + q5))))/pow((3.33333 + 1.0*cos(q5)*sin(q4) +
  cos(q4)*(1.0 + 1.0*sin(q5))), 2);
- gsl_matrix_set(C2, 1, 1, value);
+	C2[1][1] = value;
 }
 
-void print_matrix(const gsl_matrix *m){
+float* sum(float *a, float *b, float *c, int dim){
+  int i;
+
+  for(i=0; i<dim; i++)
+    c[i] = a[i] + b[i];
+  return c;
+}
+
+float* sub(float *a, float *b, float *c, int dim){
+  int i;
+
+  for(i=0; i<dim; i++)
+    c[i] = a[i] - b[i];
+  return c;
+}
+
+float* mul(float *x, float *y, int d1, int d2, float A[d1][d2]){
+  int i, j;
+
+  for(i=0; i<d1; i++){
+    y[i] = 0;
+    for(j=0; j<d2; j++)
+      y[i] += A[i][j]*x[j];
+  }
+  return y;
+}
+
+void print_matrix(int row, int column, float m[row][column]){
 
     int i, j;
 
-    for (i = 0; i < m->size1; i++){
-        for (j = 0; j < m->size2; j++)
-            printf("%g ", gsl_matrix_get(m, i, j));
+    for (i = 0; i < row; i++){
+        for (j = 0; j < column; j++)
+            printf("%g ", m[i][j]);
         printf("\n");
     }
 }
-/*
+
 int main(){
 	int i;
+
 	state rob;
+	dot_state dot_rob;
+
 	rob.q1 = rob.q2 = rob.q3 = rob.q4 = rob.q5 = rob.q6 = 1;
-	rob.dotq1 = rob.dotq2 = rob.dotq3 = rob.dotq4 = rob.dotq5 = rob.dotq6 = 1;
+	dot_rob.dq1 = dot_rob.dq2 = dot_rob.dq3 = dot_rob.dq4 = dot_rob.dq5 = dot_rob.dq6 = 1;
 
-	gsl_matrix *Tsee, *M1, *C1, *S2, *M2, *C2;
-	gsl_vector *G1, *G2;
+	float Tsee[4][4], M1[2][2], C1[2][2], S2[4][2], M2[2][2], C2[2][2];
+	float G1[2], G2[2];
 
-	Tsee = gsl_matrix_alloc(4,4);
+	/*Tsee = gsl_matrix_alloc(4,4);
 	M1 = gsl_matrix_alloc(2,2);
 	M2 = gsl_matrix_alloc(2,2);
 	C1 = gsl_matrix_alloc(2,2);
 	S2 = gsl_matrix_alloc(4,2);
 	C2 = gsl_matrix_alloc(2,2);
 	G1 = gsl_vector_alloc(2);
-	G2 = gsl_vector_alloc(2);
+	G2 = gsl_vector_alloc(2);*/
 
 	update_S2(S2, rob);
-	print_matrix(S2);
+	print_matrix(4, 2, S2);
 
-	for(i = 0; i < 1000000; i++){
+	/*for(i = 0; i < 1000000; i++){
 		update_kyn(Tsee, rob);
 		update_M1(M1, rob);
 		update_C1(C1, rob);
@@ -2251,7 +2274,7 @@ int main(){
 		rob.q4++;
 		rob.q5++;
 		rob.q6++;
-	}
+	}*/
 
 	return 0;
-}*/
+}
