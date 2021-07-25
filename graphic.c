b97c565d59ae9e16ab9a1a70465fdcdd3843d8b3
Cionix90 <cionix90@gmail.com>
2021-07-25 08:20:39 +0000

    aggiornamento del grfico della reward

--- graphic.c
+++ graphic.c
@@ -6,30 +6,32 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_blas.h>
 
-#define W_win 640
-#define H_win 480
-#define X1 120
-#define X2 520
+#define W_win 640 //larghezza della finestra
+#define H_win 480 //altezza della finestra 
+#define X1 120 //posizioni di riferimento delle BITMAP che compongono la parte
+#define X2 520 // grafica
 #define Y1 300
 #define BKG 0
-#define CR_CMP_R 160
+#define CR_CMP_R 160//colori della bitmap contenente il robot
 #define CR_CMP_G 82
 #define CR_CMP_B 45
 #define CR_All_R 105
 #define CR_All_G 105
 #define CR_All_B 105
-#define scale 2
+#define scale 2//fattore di scala
 
-//valori di OFFSET bitmap MQ e P_data
-#define W_mq 16
+//valori di OFFSET bitmap MQ 
+#define W_mq 16 //dimensioni di ogni cella della matrice Q
 #define H_mq 7
-#define X_OFF 37
+#define X_OFF 37 //offset delle celle della matrice q
 #define Y_OFF 15
-#define X_TEXT 35
+
+#define X_TEXT 35//valori di offset della stringa "matrice Q"
 #define Y_TEXT 5
-#define X_TEXT_data 15
-#define Y_TEXT_data 50
-#define FB 10
+
+#define X_TEXT_data 15//valori di offset per il plot dei 
+#define Y_TEXT_data 50//dati di interesse(parametri di learning,...)
+#define FB 10//offset di "a capo" qunado stampo i dati
 
 //elementi per aggiornamento della tabella degli stati
 #define N_ST_SV 5
@@ -40,15 +42,26 @@
 #define L_S_rect 25
 #define C_S_rect 13
 #define C_S_RAD 10
-#define G_X_OFF 15
-#define G_Y_OFF 170
-#define Len_Ax_X 300
-#define Len_Ax_Y 140
+//elementi per l'aggiornamento del grafico 
+#define G_X_OFF 50
+#define G_Y_OFF 165
+#define Len_Ax_X 301
+#define Len_Ax_Y 141
+#define X_MAX_R_L 20
+#define Y_MAX_R_L 30
+#define X_MIN_R_L 20
+#define Y_MIN_R_L 160
+#define X_EPOCH_L 300
+#define Y_EPOCH_L 170
+#define X_G_NAME 100
+#define Y_G_NAME 10
+
 //elementi da togliere perchè nella define di qlearning
 #define N_state 36
 #define N_state_x_ang 6
 #define N_action 4
 
+//parametri per il disegno del crawler più le dimensioni fisiche
 #define h_floor 50
 #define w_centre 150
 #define d_body 0.2
@@ -74,7 +87,7 @@ struct DOF state;
 
 static int figure[12];
 static int Stat_lp[N_ST_SV];
-static int Reward_lp[Len_Ax_X];
+static int Reward_p[Len_Ax_X*scale];
 static int Sl_count=0;
 static int Rew_count=0;
 static BITMAP * CR;
@@ -181,27 +194,41 @@ void update_data(float alpha,
     textout_ex(P_data, font, str, X_TEXT_data*scale, Y_TEXT_data*scale, makecol(
 0,0,0), makecol(255,255,255));
     sprintf(str,">Discount Factor:%.4f",gam);
-    textout_ex(P_data, font, str, X_TEXT_data*scale, Y_TEXT_data*2*scale, makecol(
+    textout_ex(P_data, font, str, X_TEXT_data*scale, (Y_TEXT_data+2*FB)*scale, makecol(
 0,0,0), makecol(255,255,255));
     
-    textout_ex(P_data, font,">Actual Exploration", X_TEXT_data*scale,3* Y_TEXT_data*scale, makecol(
+    textout_ex(P_data, font,">Actual Exploration", X_TEXT_data*scale, (Y_TEXT_data+3*FB)*scale, makecol(
 0,0,0), makecol(255,255,255));
     sprintf(str,"Probability:%.4f",eps);
-    textout_ex(P_data, font, str, X_TEXT_data*scale,(3* Y_TEXT_data+FB)*scale, makecol(
+    textout_ex(P_data, font, str, X_TEXT_data*scale, (Y_TEXT_data+4*FB)*scale, makecol(
 0,0,0), makecol(255,255,255));
    
-    textout_ex(P_data, font,">Decay Rate for", X_TEXT_data*scale,4* Y_TEXT_data*scale, makecol(
+    textout_ex(P_data, font,">Decay Rate for", X_TEXT_data*scale, (Y_TEXT_data+5*FB)*scale, makecol(
 0,0,0), makecol(255,255,255));
     sprintf(str," Epsilon:%.4f",decay);
-    textout_ex(P_data, font, str,X_TEXT_data*scale,(4*Y_TEXT_data+FB)*scale, makecol(
+    textout_ex(P_data, font, str,X_TEXT_data*scale, (Y_TEXT_data+6*FB)*scale, makecol(
 0,0,0), makecol(255,255,255));
 sprintf(str,">Distance:%.4f",dis);
-    textout_ex(P_data, font, str, X_TEXT_data*scale, 5*Y_TEXT_data*scale, makecol(
+    textout_ex(P_data, font, str, X_TEXT_data*scale, (Y_TEXT_data+7*FB)*scale, makecol(
 0,0,0), makecol(255,255,255));
     sprintf(str,">Epoch:%d",epoch);
-    textout_ex(P_data, font, str, X_TEXT_data*scale, 6*Y_TEXT_data*scale, makecol(
+    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+8*FB), makecol(
+0,0,0), makecol(255,255,255));
+/*
+sprintf(str,">Deadline task1:%d",dl1);
+    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+9*FB), makecol(
+0,0,0), makecol(255,255,255));
+sprintf(str,">Deadline task2:%d",dl2);
+    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+10*FB), makecol(
+0,0,0), makecol(255,255,255));
+sprintf(str,">Deadline task1:%d",dl3);
+    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+11*FB), makecol(
+0,0,0), makecol(255,255,255));
+sprintf(str,">Deadline task1:%d",dl4);
+    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+12*FB), makecol(
 0,0,0), makecol(255,255,255));
 
+*/
 
 blit(P_data,screen,0,0,X2*scale,0,P_data->w,P_data->h);
 }
@@ -244,43 +271,57 @@ void update_STAT(int new_state)
     }
     
 }
-void update_graph(float reward,int flag,int min_range,int max_range){
+
+void update_graph(float reward,int min_range,int max_range){
+   int i;
+   float val;
+   char s[10];
    line(GRP_STAT,G_X_OFF*scale,G_Y_OFF*scale,G_X_OFF*scale,(G_Y_OFF-Len_Ax_Y)*scale,makecol(0,0,0));
    line(GRP_STAT,G_X_OFF*scale,G_Y_OFF*scale,(G_X_OFF+Len_Ax_X)*scale,G_Y_OFF*scale,makecol(0,0,0));
-   if(flag==1)
-   {
-    if(Rew_count>Len_Ax_X)
+   sprintf(s,"%d",max_range);
+   textout_ex(GRP_STAT, font, s, X_MAX_R_L*scale, Y_MAX_R_L*scale, makecol(
+0,0,0), makecol(255,255,255));
+sprintf(s,"%d",min_range);
+   textout_ex(GRP_STAT, font, s, X_MIN_R_L*scale, Y_MIN_R_L*scale, makecol(
+0,0,0), makecol(255,255,255));
+
+   textout_ex(GRP_STAT, font, "Epoch", X_EPOCH_L*scale, Y_EPOCH_L*scale, makecol(
+0,0,0), makecol(255,255,255));
+       textout_ex(GRP_STAT, font, "Reward Plot", X_G_NAME*scale, Y_G_NAME*scale, makecol(
+0,0,0), makecol(255,255,255));
+
+    if(Rew_count>=Len_Ax_X*scale-1)
     {
-        for(int i=0;i<Len_Ax_X;i++)
-        {
-            Reward_lp[i]=-1;
-        } 
+         Rew_count=1;
+         Reward_p[0]=Reward_p[Len_Ax_X*scale-1];
     }
-    else
+    Reward_p[Rew_count]=reward;
+    Rew_count++;
+    for(i=0;i<Rew_count;i++)
     {
-        if(reward>max_range)
-            Reward_lp[Rew_count]=max_range;
-        else if(reward<min_range)
-            Reward_lp[Rew_count]=min_range;
+        if(Reward_p[i]>=max_range)
+            val=(float)max_range;
+        if(Reward_p[i]<=min_range)
+            val=(float)min_range;
         else
-            Reward_lp[Rew_count]=reward;
-        for(int i=0;i<Len_Ax_X;i++)
-        {
-            if(Reward_lp[i]>0)
-            {
-                
-            }
-        } 
+            val=Reward_p[i];
+        
+        val=((val-min_range)/(max_range-min_range))*(Len_Ax_Y*scale-1);
+        putpixel(GRP_STAT,(G_X_OFF*scale+1+i),(G_Y_OFF*scale-floor(val)),makecol(255,0,0));
+        
     }
-   }
+   
 }
-void update_GRP_STAT(int state)
+
+void update_GRP_STAT(int state,float reward,int max_r,int min_r,int flag)
 {
-    clear_to_color(GRP_STAT,makecol(255,255,255));
-    update_STAT(state);
-    update_graph(10,1);
-    blit(GRP_STAT,screen,0,0,X1*scale,Y1*scale,GRP_STAT->w,GRP_STAT->h);
-    
+    if(flag==1)
+    {
+        clear_to_color(GRP_STAT,makecol(255,255,255));
+        update_STAT(state);
+        update_graph(reward,min_r,max_r);
+        blit(GRP_STAT,screen,0,0,X1*scale,Y1*scale,GRP_STAT->w,GRP_STAT->h);
+    }
 }
 //funzione di utilita per il calcolo di una posizione su un Corpo rigido 
 void calc_pos(double x,double y, gsl_matrix* R,gsl_vector* p,gsl_vector*ris)
@@ -370,11 +411,7 @@ void L1_kin(int position[])
     position[3]= MToPx( (0.015 + 0.03 *cos(state.q3) + 0.1* sin(state.q3)) + (0.1 + 
       0.06* cos(state.q4))* sin(state.q3) + cos(state.q3) *(0.015 + 0.06* sin(state.q4)),1);
       
-   /* printf("L1x:%f\n",cos(state.q3)*(0.1 + 0.06 *cos(state.q4)) + 
-    (-0.1 + 0.1* cos(state.q3) - 0.03*sin(state.q3)) - 
-   sin(state.q3)* (0.015 + 0.06 *sin(state.q4)));
-      printf("L1y:%f\n", (0.015 + 0.03 *cos(state.q3) + 0.1* sin(state.q3)) + (0.1 + 
-      0.06* cos(state.q4))* sin(state.q3) + cos(state.q3) *(0.015 + 0.06* sin(state.q4)));*/
+  
 }
 void L2_kin(int position[])
 {
@@ -397,17 +434,13 @@ void L2_kin(int position[])
    cos(state.q3)* (0.015 - 0.06 *cos(state.q4)* cos(state.q5) + 
       sin(state.q4)* (0.06 + 0.06 *sin(state.q5)))
       ,1);
-     /* printf("L2x:%f\n",(-0.1  + 0.1* cos(state.q3) - 0.03* sin(state.q3)) + 
-   cos(state.q3) *(0.1 + 0.06* cos(state.q5) *sin(state.q4) + 
-      cos(state.q4) *(0.06 + 0.06 *sin(state.q5))) - 
-   sin(state.q3)* (0.015 - 0.06 *cos(state.q4)* cos(state.q5) + 
-      sin(state.q4) *(0.06 + 0.06 *sin(state.q5))));
-      printf("L2y:%f\n",(0.015 + 0.03* cos(state.q3) + 0.1* sin(state.q3)) + 
-   sin(state.q3)* (0.1 + 0.06* cos(state.q5)* sin(state.q4) + 
-      cos(state.q4)* (0.06 + 0.06 *sin(state.q5))) + 
-   cos(state.q3)* (0.015 - 0.06 *cos(state.q4)* cos(state.q5) + 
-      sin(state.q4)* (0.06 + 0.06 *sin(state.q5))));*/
+     
 }
+/*
+*Funzione che aggiorna la figura del CR mediante i dati elaborati 
+*dalla cinematica 
+
+*/
 void update_CR()
 {   
     body_kin(figure);
@@ -422,6 +455,11 @@ void update_CR()
     blit(CR,screen,0,0,X1*scale,0,CR->w,CR->h);
 
 }
+/*
+*Funzione che aggiorna la BITMAP contenente la Matrice Q, che dovrà essere 
+*passsata come argomento della funzione sotto forma di puntatore;
+*è possibile variare la gradazione dei color 
+*/
 void update_MQ(float * matrix,float step)
 {
     int i,j,val;
@@ -461,12 +499,33 @@ void update_MQ(float * matrix,float step)
     }
     blit(MQ,screen,0,0,0,0,MQ->w,MQ->h);
 }
-void update_graphic()
+/*
+void *update_graphic(void *arg)
 {
-    
-    
-    
-}
+    /*
+    *
+    *
+    int ti;
+    ti = pt_get_index(arg);
+    pt_set_activation(ti);
+    while (1)
+    {
+        /*
+        * sezione con le get per ogni struttura utilizzata in 
+        *mutua esclusione :
+        *stato,reward,variabili di learning, MAX_MIN struct,
+        *Matrice Q 
+        */
+        /*
+        *Chiamata per ogni sottofunzione a cui sono
+        *passati i valori delle struct prese 
+        *
+        if (pt_deadline_miss(ti))
+            // <do action>;
+        pt_wait_for_period(ti);
+    } 
+}*/
+
 // main esistente solo per il testing 
 int main()
 {
@@ -475,16 +534,17 @@ int main()
     update_CR();
     update_MQ((float*)&MQ_,0.5);
     update_data(0.5,0,20.2,0,0,0);
-    update_GRP_STAT(10);
-    update_GRP_STAT(11);
-    update_GRP_STAT(12);
-    update_GRP_STAT(13);
-    update_GRP_STAT(14);
-    update_GRP_STAT(15);
-    update_GRP_STAT(16);
-    update_GRP_STAT(17);
-    update_GRP_STAT(18);
-    update_GRP_STAT(19);
+   // update_GRP_STAT(int state,float reward,int max_r,int min_r,int flag)
+    update_GRP_STAT(10,10,200,0,1);
+    update_GRP_STAT(11,11,100,0,1);
+    update_GRP_STAT(12,12,100,0,1);
+    update_GRP_STAT(13,13,100,0,1);
+    update_GRP_STAT(14,14,100,0,1);
+    update_GRP_STAT(15,15,100,0,1);
+    update_GRP_STAT(16,16,100,0,1);
+    update_GRP_STAT(17,17,100,0,1);
+    update_GRP_STAT(18,18,100,0,1);
+    update_GRP_STAT(19,19,200,-200,1);
     do{
         scanf("%d",&a);
     }while(a==0); 
