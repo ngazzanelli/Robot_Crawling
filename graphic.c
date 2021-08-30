013a94e462c29c9e6ad147544b4ff3da1cd14156
Cionix90 <cionix90@gmail.com>
2021-08-30 08:44:58 +0000

    Aggiornamento quasi completo devono essere definite alcune strutture globali come la reward, le variabili di learning e poi si deve definire l'accesso alle variabili per il conteggio delle deadline.

--- graphic.c
+++ graphic.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 #include "ptask.h"
 #include <string.h>
-//#include <gsl/gsl_matrix.h>
-//#include <gsl/gsl_blas.h>
+
 #include "matrices.h"
 
 #define W_win 640 //larghezza della finestra
@@ -70,29 +69,16 @@
 #define r_wheel 0.015
 #define r_joint 0.0075
 
-extern float* get_state();
 
-//inserisco altre define che identificano la forma dei parametri da cercare 
-struct DOF{
-    float q1;
-    float q2;
-    float q3;
-    float q4;
-    float q5;
-    float q6;
-};
+//Funzioni dall'interprete
+extern int get_stop();
+extern int get_pause();
+extern int get_play();
+//funzioni per l'accesso alle variabilili globali 
+extern void get_state(state* s);
 
-//struct DOF state;
+
 state joint_var;
-
-
-static int figure[12];
-
-
-static BITMAP * CR;
-static BITMAP * MQ;
-static BITMAP * P_data;
-static BITMAP* GRP_STAT;
 static float MQ_[N_state][N_action];
 int conv_col(int col,int cscale)
 {
@@ -107,37 +93,12 @@ int conv_col(int col,int cscale)
 }
 void   init_s()
 {
-    char s[20];
-    int col,VB,VR,VG,c_scale=1,i,j;
+    int i,j;
     //inizializzazione finestra allegro 
     allegro_init();
     set_color_depth(32);
     set_gfx_mode(GFX_AUTODETECT_WINDOWED,W_win*scale,H_win*scale,0,0);
     clear_to_color(screen,BKG);
-    CR=create_bitmap((X2-X1)*scale,Y1*scale);
-    VB=255;
-    VR=255;//valore da settare
-    VG=conv_col(VR,c_scale);
-    col=makecol(VR,VG,VB);
-    clear_to_color(CR,makecol(255,255,255));
-    line(CR,0,(CR->h-h_floor*scale),(CR->w),(CR->h-h_floor*scale),1);
-    blit(CR,screen,0,0,X1*scale,0,CR->w,CR->h);
-    MQ=create_bitmap(X1*scale,Y1*scale);
-    clear_to_color(MQ,makecol(255,255,255));
-    blit(MQ,screen,0,0,0,0,MQ->w,MQ->h);
-    P_data=create_bitmap((W_win-X2)*scale,Y1*scale);
-    clear_to_color(P_data,makecol(255,255,255));
-    blit(P_data,screen,0,0,X2*scale,0,P_data->w,P_data->h);
-    
-    GRP_STAT=create_bitmap((W_win-X1)*scale,(Y1)*scale);
-    printf("%d\n",W_win);
-    clear_to_color(GRP_STAT,makecol(255,255,255));
-    blit(GRP_STAT,screen,0,0,X1*scale,Y1*scale,GRP_STAT->w,GRP_STAT->h);
-    //inizializzazione vettore ciclico S_loop
-    /*for(i=0;i<N_ST_SV;i++)
-    {
-        Stat_lp[i]=-1;
-    }*/
     // parte di inizializzazione pre-extern variable
     joint_var.q1=0;
     joint_var.q2=0;
@@ -168,14 +129,10 @@ void update_data(BITMAP* BM_TXT,
                 float eps,
                 float decay,
                 float dis,
-                int epoch ){
+                int epoch )
+{
     char str[25];
-/*
-    static float alpha;		// learning rate    
-    static float gam;		// discount factor
-    static float epsilon; 	// actual exploration probability
-    static float decay;		// decay rate for epsilon
-    */
+    clear_to_color(BM_TXT,makecol(255,255,255));
     sprintf(str,">Learning Rate:%.4f",alpha);
     textout_ex(BM_TXT, font, str, X_TEXT_data*scale, Y_TEXT_data*scale, makecol(0,0,0), makecol(255,255,255));
     sprintf(str,">Discount Factor:%.4f",gam);
@@ -190,23 +147,23 @@ void update_data(BITMAP* BM_TXT,
     textout_ex(BM_TXT, font, str, X_TEXT_data*scale, (Y_TEXT_data+7*FB)*scale, makecol(0,0,0), makecol(255,255,255));
     sprintf(str,">Epoch:%d",epoch);
     textout_ex(BM_TXT, font, str, X_TEXT_data*scale,  (Y_TEXT_data+8*FB), makecol(0,0,0), makecol(255,255,255));
-/*
-sprintf(str,">Deadline task1:%d",dl1);
-    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+9*FB), makecol(
-0,0,0), makecol(255,255,255));
-sprintf(str,">Deadline task2:%d",dl2);
-    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+10*FB), makecol(
-0,0,0), makecol(255,255,255));
-sprintf(str,">Deadline task1:%d",dl3);
-    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+11*FB), makecol(
-0,0,0), makecol(255,255,255));
-sprintf(str,">Deadline task1:%d",dl4);
-    textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+12*FB), makecol(
-0,0,0), makecol(255,255,255));
+    /*
+    sprintf(str,">Deadline task1:%d",dl1);
+        textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+9*FB), makecol(
+    0,0,0), makecol(255,255,255));
+    sprintf(str,">Deadline task2:%d",dl2);
+        textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+10*FB), makecol(
+    0,0,0), makecol(255,255,255));
+    sprintf(str,">Deadline task1:%d",dl3);
+        textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+11*FB), makecol(
+    0,0,0), makecol(255,255,255));
+    sprintf(str,">Deadline task1:%d",dl4);
+        textout_ex(P_data, font, str, X_TEXT_data*scale,  (Y_TEXT_data+12*FB), makecol(
+    0,0,0), makecol(255,255,255));
 
-*/
+    */
 
-blit(BM_TXT,screen,0,0,X2*scale,0,P_data->w,P_data->h);
+    blit(BM_TXT,screen,0,0,X2*scale,0,BM_TXT->w,BM_TXT->h);
 }
 
 
@@ -217,12 +174,12 @@ void update_STAT(BITMAP* BM_SG,int new_state)
     int i,j,k,col;
     Stat_lp[Sl_count]=new_state;
     Sl_count=(Sl_count+1)%N_ST_SV;
-    textout_ex(GRP_STAT, font, "State Matrix", X_Lab_S_OFF*scale, Y_Lab_S_OFF*scale, makecol(0,0,0), makecol(255,255,255));
+    textout_ex(BM_SG, font, "State Matrix", X_Lab_S_OFF*scale, Y_Lab_S_OFF*scale, makecol(0,0,0), makecol(255,255,255));
     for(i=0;i<N_state_x_ang;i++)
     {
         for(j=0;j<N_state_x_ang;j++)
         {
-            rect(GRP_STAT,(X_Mat_S_OFF+i*L_S_rect)*scale,
+            rect(BM_SG,(X_Mat_S_OFF+i*L_S_rect)*scale,
             (Y_Mat_S_OFF+j*L_S_rect)*scale,
             (X_Mat_S_OFF+(i+1)*L_S_rect)*scale,
             (Y_Mat_S_OFF+(j+1)*L_S_rect)*scale,
@@ -233,7 +190,7 @@ void update_STAT(BITMAP* BM_SG,int new_state)
                 if(Stat_lp[k]==(i*N_state_x_ang+j))
                 {
                     col=245*k/N_ST_SV;
-                    circlefill(GRP_STAT,
+                    circlefill(BM_SG,
                     (X_Mat_S_OFF+i*L_S_rect+C_S_rect)*scale,
                     (Y_Mat_S_OFF+j*L_S_rect+C_S_rect)*scale,
                     C_S_RAD,
@@ -303,8 +260,17 @@ void update_GRP_STAT(BITMAP* BM_GS,int state,float reward,int max_r,int min_r,in
         blit(BM_GS,screen,0,0,X1*scale,Y1*scale,BM_GS->w,BM_GS->h);
     }
 }
-//funzione per il calcolo dei PX per disegnare il corpo, a seguire c'è
-//quella per il link 1 e poi per il secondo
+/*
+* set di funzioni per la gestione della bitmap in cui viene 
+* disegnato il crawler.
+* MToPx mappa le coordinate reali del modello nei pixel del disegno 
+*
+* ""_kin aggiornano il vettore position con i punti necessari ad allegro 
+* per disegnare il crawler;  
+* 
+* update_crawler va ad utilizzare le precedenti funzioni per disegnare 
+* effettivamente sulla bitmap il soggetto
+*/
 int MToPx(double val,int xy)
 {
     if (xy==0)
@@ -313,7 +279,7 @@ int MToPx(double val,int xy)
     }
     if(xy==1)
     {
-        return(CR->h-((int)floor(val*1000)+h_floor)*scale);
+        return((Y1*scale)-((int)floor(val*1000)+h_floor)*scale);
     }
     else{
         
@@ -384,43 +350,36 @@ void L2_kin(int position[],state s)
      
 }
 
-void init_CR(BITMAP*BM_CR)
-{
-    BM_CR=create_bitmap((X2-X1)*scale,Y1*scale);
-    clear_to_color(BM_CR,makecol(255,255,255));
-    line(BM_CR,0,(BM_CR->h-h_floor*scale),(CR->w),(CR->h-h_floor*scale),1);
-    blit(CR,screen,0,0,X1*scale,0,CR->w,CR->h);
-}
 void update_CR(BITMAP* BM_CR,state joint_v)
 {   
+    int figure[12];
+    clear_to_color(BM_CR,makecol(255,255,255));
+    line(BM_CR,0,(BM_CR->h-h_floor*scale),(BM_CR->w),(BM_CR->h-h_floor*scale),1);
     body_kin(figure,joint_v);
-    polygon(CR,5,figure,makecol(CR_CMP_R,CR_CMP_G,CR_CMP_B));
-    circlefill(CR,figure[10],figure[11],MToPx(r_wheel,2),makecol(CR_All_R,CR_All_G,CR_All_B));
-    circlefill(CR,figure[4],figure[5],MToPx(r_joint,2),makecol(CR_All_R,CR_All_G,CR_All_B));
+    polygon(BM_CR,5,figure,makecol(CR_CMP_R,CR_CMP_G,CR_CMP_B));
+    circlefill(BM_CR,figure[10],figure[11],MToPx(r_wheel,2),makecol(CR_All_R,CR_All_G,CR_All_B));
+    circlefill(BM_CR,figure[4],figure[5],MToPx(r_joint,2),makecol(CR_All_R,CR_All_G,CR_All_B));
    L1_kin(figure,joint_v);
-    line(CR,figure[0],figure[1],figure[2],figure[3],makecol(CR_CMP_R,CR_CMP_G,CR_CMP_B));
-    circlefill(CR,figure[2],figure[3],MToPx(r_joint,2),makecol(CR_All_R,CR_All_G,CR_All_B));
+    line(BM_CR,figure[0],figure[1],figure[2],figure[3],makecol(CR_CMP_R,CR_CMP_G,CR_CMP_B));
+    circlefill(BM_CR,figure[2],figure[3],MToPx(r_joint,2),makecol(CR_All_R,CR_All_G,CR_All_B));
     L2_kin(figure,joint_v);
-    line(CR,figure[0],figure[1],figure[2],figure[3],makecol(CR_CMP_R,CR_CMP_G,CR_CMP_B));
+    line(BM_CR,figure[0],figure[1],figure[2],figure[3],makecol(CR_CMP_R,CR_CMP_G,CR_CMP_B));
     
-    blit(CR,screen,0,0,X1*scale,0,CR->w,CR->h);
+    blit(BM_CR,screen,0,0,X1*scale,0,BM_CR->w,BM_CR->h);
 
 }
 /*
-*Funzione che aggiorna la BITMAP contenente la Matrice Q, che dovrà essere 
-*passsata come argomento della funzione sotto forma di puntatore;
-*è possibile variare la gradazione dei color 
+* Funzione che aggiorna la BITMAP contenente la Matrice Q, che dovrà essere 
+* passsata come argomento della funzione sotto forma di puntatore;
+* è possibile variare la gradazione dei colori con cui vengono 
+* rappresetnati i valori della matrice graficamente mediante 
+* l'argomento <step> della update
 */
-void init_MQ(BITMAP* BM_MQ)
-{
-    BM_MQ=create_bitmap(X1*scale,Y1*scale);
-    clear_to_color(BM_MQ,makecol(255,255,255));
-    blit(BM_MQ,screen,0,0,0,0,BM_MQ->w,BM_MQ->h);
-}
+
 void update_MQ(BITMAP* BM_MQ,float * matrix,float step)
 {
     int i,j,val;
-    
+    clear_to_color(BM_MQ,makecol(255,255,255));
     textout_ex(BM_MQ, font, "Matrice Q", X_TEXT*scale, Y_TEXT*scale, makecol(0,0,0), makecol(255,255,255));
     for(i=0;i<N_state;i++)
     {
@@ -457,30 +416,37 @@ void update_MQ(BITMAP* BM_MQ,float * matrix,float step)
     blit(BM_MQ,screen,0,0,0,0,BM_MQ->w,BM_MQ->h);
 }
 
+
 /*
 void *update_graphic(void *arg)
 {
-    /*
-    *
-    *
+   
     int ti;
+    state rob;
+    BITMAP *CR,*MQ,*P_data,*GRP_STAT;
+    //inizializzo allegro e lo scermo 
+    init_s();
+    //inizializzo le BITMAP
+    CR=create_bitmap((X2-X1)*scale,Y1*scale);
+    MQ=create_bitmap(X1*scale,Y1*scale);
+    P_data=create_bitmap((W_win-X2)*scale,Y1*scale);
+    GRP_STAT=create_bitmap((W_win-X1)*scale,(Y1)*scale);
+
     ti = pt_get_index(arg);
     pt_set_activation(ti);
-    while (1)
+    while (!get_stop())
     {
-        /*
-        * sezione con le get per ogni struttura utilizzata in 
-        *mutua esclusione :
-        *stato,reward,variabili di learning, MAX_MIN struct,
-        *Matrice Q 
-        */
-        /*
-        *Chiamata per ogni sottofunzione a cui sono
-        *passati i valori delle struct prese 
-        *
-        if (pt_deadline_miss(ti))
-            // <do action>;
+        if(get_play())
+        {
+
+            get_state(&rob);
+            update_CR(CR,rob);
+
+        }
+        
+        pt_deadline_miss(ti);
         pt_wait_for_period(ti);
+        
     } 
 }*/
 
@@ -489,7 +455,14 @@ void *update_graphic(void *arg)
 int main()
 {
     int a;
+    float prova;
+    BITMAP *CR,*MQ,*P_data,*GRP_STAT;
     init_s();
+
+    CR=create_bitmap((X2-X1)*scale,Y1*scale);
+    MQ=create_bitmap(X1*scale,Y1*scale);
+    P_data=create_bitmap((W_win-X2)*scale,Y1*scale);
+    GRP_STAT=create_bitmap((W_win-X1)*scale,(Y1)*scale);
     update_CR(CR,joint_var);
     update_MQ(MQ,(float*)&MQ_,0.5);
     update_data(P_data,0.5,0,20.2,0,0,0);
@@ -505,6 +478,14 @@ int main()
     update_GRP_STAT(GRP_STAT,18,18,100,0,1);
     update_GRP_STAT(GRP_STAT,19,19,200,-200,1);
 
+    do{
+        scanf("%d",&a);
+    }while(a==0); 
+    for(prova=-200;prova<200;prova=prova+0.1)
+    {
+         update_GRP_STAT(GRP_STAT,19,prova,200,-200,1);
+    }
+    a=1;
     do{
         scanf("%d",&a);
     }while(a==0); 
